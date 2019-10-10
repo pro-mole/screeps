b639949721d211c445f1a63032926131ae93d0e0
pro-mole <guedesav@gmail.com>
2019-10-10 13:17:14 -0300

    Syntax fix and signature fix

--- task/harvest.js
+++ task/harvest.js
@@ -1,15 +1,15 @@
 module.exports = {
-    init = function() {
+    init: function(data) {
         // If no source defined, find a source
         // State => To Source
     },
-    state_check = function() {
+    state_check: function() {
         // If adjacent to the source and not Harvesting => Harvesting
         // If harvesting and full => To Destination
         // If adjacent to destination and not empty => Dumping
         // If dumping and empty => DONE
     },
-    run = function() {
+    run: function() {
         // To Source: move towards source
         // Harvesting: gather energy
         // To Destination: move towards destination
