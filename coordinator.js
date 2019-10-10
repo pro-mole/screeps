160cb447559fc0b645690d26e5b428b66fe166e7
pro-mole <guedesav@gmail.com>
2019-10-10 13:16:03 -0300

    Syntax fix

--- coordinator.js
+++ coordinator.js
@@ -2,28 +2,28 @@
 
 
 module.exports = {
-    workers = {}, // Worker list by type
-    sources = {}, // Sources of energy
-    center = {}, // Control centers
+    workers: {}, // Worker list by type
+    sources: {}, // Sources of energy
+    center: {}, // Control centers
 
     /// Take actions given the current conditions
-    coordinate = function() {
+    coordinate: function() {
         // DRAFT - If there are no workers, spawn a worker and set it to harvest
     },
     /// Assign tasks to idle workers
-    delegate = function() {
+    delegate: function() {
         // DRAFT - If a worker is idle, set it to harvest
     },
     /// Assess the state of the world, updating internal data
-    assess = function() {
+    assess: function() {
     },
 
     /// Spawn a worker of the given worker type
-    spawn = function(workerType) {
+    spawn: function(workerType) {
 
     },
     /// Assign a task to a worker, with given data (if relevant)
-    giveTask = function(worker, rulebook, taskData) {
+    giveTask: function(worker, rulebook, taskData) {
 
     }
 }
